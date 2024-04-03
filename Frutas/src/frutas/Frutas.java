2276a54e468d796ad5141bf061e2d97ddaba3c11
idiomas <idiomas@SJ106PC3.fup.edu>
2024-04-03 12:59:21 -0500

    Clase Frutas

---
+++ Frutas/src/frutas/Frutas.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package frutas;
+
+/**
+ *
+ * @author idiomas
+ */
+public class Frutas {
+        private int numero;
+        private int tipo;
+        private double tarifa;
+        private double valorFinal;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Atributos Clase Cita
+        
+    }
+    
+}

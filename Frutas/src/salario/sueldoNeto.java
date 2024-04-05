81b146b1ebfd4fbbbd02f2087528f13e74cc276b
idiomas <idiomas@SJ106PC3.fup.edu>
2024-04-04 19:38:08 -0500

    Nuevo Ejercicio

---
+++ Frutas/src/salario/sueldoNeto.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package salario;
+
+/**
+ *
+ * @author idiomas
+ */
+public class sueldoNeto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

484186c097b49b50e3d7f6fe8f6869fa1106af8e
idiomas <idiomas@SJ106PC3.fup.edu>
2024-04-04 19:35:37 -0500

    mensaje de saludo

--- Frutas/src/frutas/Salario.java
+++ Frutas/src/frutas/Salario.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package frutas;
 
-/**
- *
- * @author idiomas
- */
 public class Salario {
 
     /**
@@ -15,6 +6,8 @@ public class Salario {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.err.println("Hola Mundo");
     }
     
 }

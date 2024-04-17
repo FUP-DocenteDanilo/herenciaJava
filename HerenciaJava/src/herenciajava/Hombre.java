2834ba4f9354e84c11d1917a34a4c5dcd3d6bbb7
idiomas <idiomas@SJ106PC3.fup.edu>
2024-04-17 12:25:15 -0500

    Ejercicio Herencia todo

---
+++ HerenciaJava/src/herenciajava/Hombre.java
@@ -0,0 +1,11 @@
+package herenciajava;
+
+public class Hombre extends Humano{
+    
+    public void afeitarce(){
+        System.out.println(getNombre()+" Esta afeitandose desde la clase Hombre");
+    
+    }
+    
+    
+}
